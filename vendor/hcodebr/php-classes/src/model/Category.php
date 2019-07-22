a592c39a9f3b5f4ad05f93b38731fdbf8ce55632
Diogo Amorim <diogo.amorim.dias@hotmail.com>
2019-07-22 12:04:32 -0300

    Admin - Products

--- vendor/hcodebr/php-classes/src/model/Category.php
+++ vendor/hcodebr/php-classes/src/model/Category.php
@@ -15,8 +15,6 @@ class Category extends Model {
         $sql = new Sql();
 
         return $sql->select("SELECT * FROM tb_categories ORDER BY descategory");
-
-
     }      
 
     public function save()
@@ -30,6 +28,8 @@ class Category extends Model {
         ));
        
         $this->setData($results[0]);    
+
+        Category::updateFile();
     }
 
     public function get($idcategory)
@@ -56,7 +56,24 @@ class Category extends Model {
 
         ]);
 
+        Category::updateFile();
 
+
+    }
+
+    public static function updateFile()
+
+    {
+        $categories = Category::listAll();
+        
+        $html = [];
+
+        foreach ($categories as $row) 
+        {
+            array_push($html,'<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR."views".DIRECTORY_SEPARATOR."categories-menu.html", implode('',$html));
     }
 
 }
